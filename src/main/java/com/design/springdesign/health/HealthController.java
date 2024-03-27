d9c5314d3fa64419b6b44733f4c99bbf5a734283
lobster <long_tao@aliyun.com>
2024-03-28 00:28:03 +0800

    【修改healthCheck文件名】

--- src/main/java/com/design/springdesign/health/HealthController.java
+++ src/main/java/com/design/springdesign/health/HealthController.java
@@ -1,4 +1,4 @@
-package com.design.springdesign.controller;
+package com.design.springdesign.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
